c8f9cceb49d08a8bf34244e1a1ca7e0cfcd7a425
Andre <post.for.andre@gmail.com>
2019-06-08 19:10:49 +0200

    Fix encoding of bytes for transmission to lorawan

--- dataCollector.php
+++ dataCollector.php
@@ -31,8 +31,14 @@ $boxIds = ['590e0b0a51d3460011c725c4', '5ca9f885695e3b001ac9245c', '5b816cce7c51
 $values = [];
 
 foreach ($boxIds as $boxId) {
+    var_dump($boxId);
+
 $url = $baseUrl . $boxId . '/sensors';
 $response = file_get_contents($url);
+var_dump($response);
+if ($response === false) {
+    continue;
+}
 $responseJson = json_decode($response, true);
 $lastValue = floatval($responseJson['sensors'][0]['lastMeasurement']['value']);
 $values[] = $lastValue;
@@ -61,8 +67,6 @@ $baseURL = 'https://www.umweltbundesamt.de/js/uaq/data/stations/measuring?statio
 
 $requestUrl = $baseURL . $timeStampPast . ',' . $timeStampNow;
 
-var_dump($requestUrl);
-
 $response = file_get_contents($requestUrl);
 
 if ($response === false) {
@@ -75,10 +79,10 @@ $responseJson = json_decode($response, true);
 $data = $responseJson['data'][0];
 
 foreach ($data as $value) {
-    if ($value[0] != false) {
+if ($value[0] != false) {
 
-        return intval($value[0]);
-    }
+return intval($value[0]);
+}
 }
 
 return 0;
@@ -98,8 +102,8 @@ $ttnpost = fgets($fp);
 fclose($fp);
 
 if ($ttnpost === false) {
-    echo "Kann lastpost.txt nicht öffnen";
-    exit;
+echo "Kann lastpost.txt nicht öffnen";
+exit;
 }
 
 return intval(str_replace('paxcount":', "", strstr(strstr($ttnpost, 'paxcount'), '}', true)));
@@ -108,70 +112,32 @@ return intval(str_replace('paxcount":', "", strstr(strstr($ttnpost, 'paxcount'),
 
 function postData() {
 
-    $url = 'https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/klimaprobe/sensor-data?key=ttn-account-v2.Ymn1B5VcoYbV0NjZ__r9pEUdfqb6AK-s5wHGCe3Hn3U';
+$url = 'https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/klimaprobe/sensor-data?key=ttn-account-v2.Ymn1B5VcoYbV0NjZ__r9pEUdfqb6AK-s5wHGCe3Hn3U';
 
-    /**
-    $sensorData = '{' . '"feinstaub": ' . getFeinstaubValue() . ', '
-    . '"stickoxid": ' . getNo2Value() . ', '
-    . '"pax": ' . getPaxCounterFromLastPostTxt()
-    ;
-    */
+$sensorData = chr(getFeinstaubValue()) . chr(getNo2Value()) . chr(getPaxCounterFromLastPostTxt());
+$sensorData = base64_encode($sensorData);
 
-    $sensorData = get_base_16_encoded_result();
+$dataJson = '{"dev_id": "rak811-2",
+"port": 1,
+"confirmed": false,
+"payload_raw": "' . $sensorData . '"}';
 
-    $dataJson = '{"dev_id": "rak811-2",
-    "port": 1,
-    "confirmed": false,
-    "payload_raw": "' . $sensorData . '"}';
+$options = array(
+'http' => array(
+'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+'method'  => 'POST',
+'content' => $dataJson
+)
+);
 
-    var_dump($dataJson);
-
-    $options = array(
-    'http' => array(
-    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-    'method'  => 'POST',
-    'content' => $dataJson
-    )
-    );
-
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) {
-        var_dump($result);
-        var_dump("something wrong");
-        /* Handle error */
-    }
-
-    var_dump($result);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) {
+//    var_dump($result);
+//  var_dump("something wrong");
+/* Handle error */
 }
 
-/**
- * @return string
- */
-function get_base_16_encoded_result() {
-
-    $feinstaub = getFeinstaubValue();
-    $stick = getNo2Value();
-    $pax = getPaxCounterFromLastPostTxt();
-
-    $raw_value_string = '';
-    foreach ([$feinstaub, $stick, $pax] as $value) {
-      //  var_dump("normal", $value);
-      //  var_dump("hex", base_convert($value, 10, 16));
-        $raw_value = base_convert($value, 10, 16);
-        if ($value <= 16) {
-            $raw_value = '0' . $raw_value;
-       //     var_dump("null angehangen", $raw_value);
-        }
-
-        $raw_value_string = $raw_value_string . $raw_value;
-    }
-  //*/
-
-    var_dump($raw_value_string);
-    return $raw_value_string;
 }
-
-//postData();
 
 postData();
